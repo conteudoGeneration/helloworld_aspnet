27e203cec449b8c5469f70cd91bdc54e77af9ca8
Rafael Queiróz <rafaelproinfo@gmail.com>
2023-07-30 16:25:27 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace helloworld.Controllers
+{
+    [Route("~/")]
+    [ApiController]
+    public class HelloController
+    {
+
+        [HttpGet]
+        public string Hello()
+        {
+            return "Hello World!";
+        }
+
+    }
+
+}
